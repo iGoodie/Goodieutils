d05d9f2244853cc17eefc7a1f79be4e3a691a151
Anılcan <xgen_blue@windowslive.com>
2018-11-18 21:51:36 +0300

    Convert into maven project

---
+++ src/com/programmer/igoodie/utils/io/CommandLineArgs.java
@@ -0,0 +1,56 @@
+package com.programmer.igoodie.utils.io;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class CommandLineArgs {
+
+	private String[] rawArgs;
+	private HashSet<String> flags;
+	private HashMap<String, String> args;
+	
+	public CommandLineArgs(String[] rawArgs) {
+		this.rawArgs = rawArgs;
+		this.flags = new HashSet<>();
+		this.args = new HashMap<>();
+		
+		//Parse args
+		for(String arg : rawArgs) {
+			if(arg.startsWith("--")) // --someflag
+				this.flags.add(arg.substring(2));
+			else if(arg.startsWith("-") && arg.contains(":")) { // -key:value
+				String[] pair = arg.substring(1).split(":", 2);
+				this.args.put(pair[0], pair[1]);
+			}
+		}
+	}
+	
+	public int getFlagCount() {
+		return flags.size();
+	}
+	
+	public int getSubargumentCount() {
+		return args.size();
+	}
+
+	public int getArgumentCount() {
+		return rawArgs.length;
+	}
+	
+	public boolean containsFlag(String flag) {
+		return flags.contains(flag);
+	}
+
+	public boolean containsArgument(String arg) {
+		return args.containsKey(arg);
+	}
+	
+	public String getArgument(String argName) {
+		return args.get(argName);
+	}
+	
+	public String getArgument(String argName, String defaultValue) {
+		String arg = args.get(argName);
+		return arg==null ? defaultValue : arg;
+	}
+}
